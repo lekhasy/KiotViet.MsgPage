b735a432cfeb56d76d2a5fa1e74e29af8bc49172
Sy LK <gemini02061994@outlook.com>
2017-09-06 08:11:09 +0700

    Add core guard

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgMaterialModule } from './ng-material/ng-material.module';
@@ -9,4 +9,18 @@ import { NgMaterialModule } from './ng-material/ng-material.module';
   declarations: [],
   exports: [NgMaterialModule, BrowserAnimationsModule]
 })
-export class CoreModule { }
+export class CoreModule {
+  /**
+   *
+   */
+  constructor(@Optional() @SkipSelf() parentModule : any){
+    this.throwIfAlreadyLoaded(parentModule, CoreModule.name);
+  }
+
+  private throwIfAlreadyLoaded(parentModule : any, moduleName: string) : void {
+    // app module don't have parent module
+    if(parentModule){
+      throw new Error('${moduleName} has already been loaded. Import Core modules in the AppModule only.');
+    }
+  }
+ }
